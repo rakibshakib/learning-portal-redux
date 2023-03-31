cc1368050ffbb40a37b39473efcf688eb1ed0af3
rakibshakib <rakibshakib74@gmail.com>
2023-03-31 16:03:43 +0600

    update: design complete from html to react

---
+++ src/module/students/Videos/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import VideoPlayer from "./VideoPlayer";
+import VideoSideBar from "./VideoSideBar";
+
+const CourseVideos = () => {
+  return (
+    <section className="py-6 bg-primary">
+      <div className="mx-auto max-w-7xl px-5 lg:px-0">
+        <div className="grid grid-cols-3 gap-2 lg:gap-8">
+          <VideoPlayer />
+          <VideoSideBar />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CourseVideos;
