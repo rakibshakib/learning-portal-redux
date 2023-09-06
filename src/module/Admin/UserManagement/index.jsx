448f05d7d8d67c7bc21e7d82ecf879d133aeba52
iboscopilot <eaccount@ibos.io>
2023-09-06 15:56:12 +0600

    update: updated layout design

--- src/module/Admin/UserManagement/index.jsx
+++ src/module/Admin/UserManagement/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import ConfirmModal from "../../../common/ConfirmModal";
 import { useNavigate } from "react-router-dom";
